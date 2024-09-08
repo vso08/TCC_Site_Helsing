c80e0b43cbf758110467ac5c7500c08d70198e28
João Vitor <jaolekal@gmail.com>
2024-09-08 17:35:20 -0300

    Escola

--- src/components/Cenarios.jsx
+++ src/components/Cenarios.jsx
@@ -2,6 +2,7 @@ import img1 from "../assets/m1.jpeg";
 import img2 from "../assets/m2.jpeg";
 import "../App.css";
 import Lobo from "../assets/Lobi.png";
+import Frank from "../assets/Frank.png"
 import { React, useEffect, useRef } from "react";
 
 function Cenario() {
@@ -23,10 +24,10 @@ function Cenario() {
   return (
     <>
       <div className="bg-zinc-800">
-        <div className="page-container pt-20">
-          <div className="content">
+        <div className="page-container">
+          <div className="content py-20">
             <div className="prev w-20" onClick={prev}></div>
-            <div className="slide-painel" ref={ImageContainerRef}>
+            <div className="slide-painel py-40 md:h-20 overflow-hidden" ref={ImageContainerRef}>
               {images.map((image) => {
                 return <img src={image} className="itemCar" key={1} />;
               })}
@@ -34,10 +35,10 @@ function Cenario() {
             <div className="next w-20" onClick={next}></div>
           </div>
         </div>
-        <div className="pt-20">
+        <div className="">
           <div className="lg:grid grid-rows-2 lg:grid-cols-2 gap-0 text-white">
             <div className="cardBoss flex md:bg-gradient-to-r from-zinc-900 to-transparent bg-gradient-to-b p-5">
-              <div className="BossFoto p-5 w-100 md:w-1/3 md:h-full w-full h-1/3 flex justify-center">
+              <div className="BossFoto dir p-5 w-100 md:w-1/3 md:h-full w-full h-1/3 flex justify-center">
                 <span><img
                   src={Lobo}
                   className="bossImg border-4 outline-red-900 outline outline-2 rounded-full p-2 " 
@@ -63,18 +64,18 @@ function Cenario() {
                   desafiando para um duelo em seu temido mundo.{" "}
                 </h2>
               </div>
-              <div className="BossFoto2  w-100 md:w-1/3 md:h-full w-full h-1/3 flex justify-center">
+              <div className="BossFoto p-5 w-100 md:w-1/3 md:h-full w-full h-1/3 flex justify-center">
                 <img
                   src={Lobo}
-                  className="border-4 outline-red-900 outline outline-2 rounded-full p-2"
+                  className="bossImg border-4 outline-red-900 outline outline-2 rounded-full p-2"
                 ></img>
               </div>
             </div>
             <div className="cardBoss flex md:bg-gradient-to-r from-zinc-900 to-transparent bg-gradient-to-b p-5">
-              <div className="BossFoto3  w-100 md:w-1/3 md:h-full w-full h-1/3 flex justify-center">
+              <div className="BossFoto dir p-5 w-100 md:w-1/3 md:h-full w-full h-1/3 flex justify-center">
                 <img
                   src={Lobo}
-                  className="border-4 outline-red-900 outline outline-2 rounded-full p-2"
+                  className="bossImg border-4 outline-red-900 outline outline-2 rounded-full p-2"
                 ></img>
               </div>
               <div className="w-full">
@@ -97,10 +98,10 @@ function Cenario() {
                   desafiando para um duelo em seu temido mundo.{" "}
                 </h2>
               </div>
-              <div className="BossFoto4  w-100 md:w-1/3 md:h-full w-full h-1/3 flex justify-center">
+              <div className="BossFoto p-5 w-100 md:w-1/3 md:h-full w-full h-1/3 flex justify-center">
                 <img
                   src={Lobo}
-                  className="border-4 outline-red-900 outline outline-2 rounded-full p-2"
+                  className="bossImg border-4 outline-red-900 outline outline-2 rounded-full p-2"
                 ></img>
               </div>
             </div>
