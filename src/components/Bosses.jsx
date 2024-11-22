a00018a5716ab3ff183d1d9153369d31cb374dab
João Vitor <jaolekal@gmail.com>
2024-11-22 14:33:08 -0300

    commit apresentacao

--- src/components/Bosses.jsx
+++ src/components/Bosses.jsx
@@ -19,13 +19,13 @@ import mor from "../assets/morcego.png";
 import rolo from "../assets/rolo.png";
 import fogo from "../assets/fogo.png";
 import gelo from "../assets/gelo.png";
-
+ 
 function Modal({ isOpen, onClose, content }) {
   if (!isOpen) return null;
-
+ 
   return (
     <>
-      <div className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none">
+      <div className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none RTDyans">
         <div className="relative w-full max-w-7xl mx-auto my-6 max-h-screen">
           <div className="relative flex flex-col w-full p-0 bg-zinc-800 rounded-lg shadow-lg outline-none focus:outline-none overflow-y-auto max-h-screen">
             <div className="flex items-start justify-between p-5 bg-zinc-950 rounded-t">
@@ -43,27 +43,29 @@ function Modal({ isOpen, onClose, content }) {
                 alt={content.altText}
                 className="object-contain w-full md:w-1/3 max-h-96 p-3 bg-zinc-900 rounded-md"
               />
-              <p className="flex-1 p-2 text-center bg-zinc-900 rounded-md md:text-lg">
+              <p className="flex-1 p-20 text-center bg-zinc-900 rounded-md md:text-lg">
                 {content.description}
               </p>
             </div>
             <div className="px-5 w-full mb-5 py-2">
               <div className="grid grid-cols-1 md:grid-cols-3 gap-4 pt-2">
-                <div className="bg-zinc-900 text-center rounded-sm p-2">
+                <div className="bg-zinc-900 text-center rounded-sm p-2 flex flex-col">
                   <p>Ataque Normal:</p>
                   <img
                     src={content.normalAttack}
                     alt="Ataque Normal"
                     className="mx-auto"
                   />
+                  <p className="border rounded-lg bg-zinc-950 mt-auto text-lg">{content.descAtaq}</p>
                 </div>
-                <div className="bg-zinc-900 text-center rounded-sm p-2">
+                <div className="bg-zinc-900 text-center rounded-sm p-2 flex flex-col">
                   <p>Ataque Especial:</p>
                   <img
                     src={content.especialAttack}
                     alt="Ataque Especial"
                     className="mx-auto"
                   />
+                  <p className="border rounded-lg bg-zinc-950 mt-auto text-lg">{content.descAtaqSp}</p>
                 </div>
                 <div className="bg-zinc-900 text-center rounded-sm p-2">
                   <p>Cenário:</p>
@@ -100,7 +102,7 @@ function Modal({ isOpen, onClose, content }) {
     </>
   );
 }
-
+ 
 Modal.propTypes = {
   isOpen: PropTypes.bool.isRequired,
   onClose: PropTypes.func.isRequired,
@@ -113,9 +115,11 @@ Modal.propTypes = {
     scenario: PropTypes.string,
     especialAttack: PropTypes.string,
     video: PropTypes.string,
+    descAtaq: PropTypes.string,
+    descAtaqSp: PropTypes.string,
   }),
 };
-
+ 
 function CardMonster({
   imageSrc,
   altText,
@@ -126,9 +130,11 @@ function CardMonster({
   ImgCenario,
   AtqEspecial,
   Video,
+  descAtaq,
+  descAtaqSp
 }) {
   const [showModal, setShowModal] = useState(false);
-
+ 
   const modalContent = {
     title: TextCard,
     imageSrc,
@@ -138,8 +144,10 @@ function CardMonster({
     scenario: ImgCenario,
     especialAttack: AtqEspecial,
     video: Video,
+    descAtaq:descAtaq,
+    descAtaqSp:descAtaqSp
   };
-
+ 
   return (
     <div className="flex p-3 text-white bg-neutral-900 rounded-lg">
       <img
@@ -149,7 +157,7 @@ function CardMonster({
       />
       <div className="p-2 text-center">
         <h1 className="text-xl bg-zinc-950 rounded-md RTDyans">{TextCard}</h1>
-        <h2 className="pt-5 text-md RTDyans">{DescCard || "Sem descrição"}</h2>
+        <h2 className="pt-5 text-md RTDyans transparent md:visible text-white">{DescCard || "Sem descrição"}</h2>
         <div className="flex justify-end p-1 buttonVM">
           <button
             className="mt-2 rounded-sm flex w-full bg-red-800 justify-center"
@@ -167,7 +175,7 @@ function CardMonster({
     </div>
   );
 }
-
+ 
 CardMonster.propTypes = {
   imageSrc: PropTypes.string.isRequired,
   altText: PropTypes.string.isRequired,
@@ -178,8 +186,10 @@ CardMonster.propTypes = {
   ImgCenario: PropTypes.string,
   AtqEspecial: PropTypes.string,
   Video: PropTypes.string,
+  descAtaq: PropTypes.string,
+  descAtaqSp: PropTypes.string
 };
-
+ 
 function Bosses() {
   return (
     <div className="bg-zinc-950" id="bosses">
@@ -196,46 +206,59 @@ function Bosses() {
             altText="Imagem do Lobisomem"
             TextCard="Lobisomem"
             DescCard="Você se depará com o Lobisomem na floresta. Qual sua reação? Correr? Gritar? Saiba que para Helsing, tudo isso é só mais um desafio."
-            DescModal="O desafio de uma das figuras mitológicas mais famosas do mundo se inicia na floresta, o primeiro cenário de Helsing. Após interagir com o vendedor, enfrente o Lobisomem!"
+            DescModal="
+O Lobisomem de Helsing é uma figura trágica, vítima de uma maldição ancestral que o transforma em uma fera sedenta de sangue sob a lua cheia. Originado do folclore europeu, ele simboliza a luta interna entre a humanidade e os instintos primitivos. Seu uivo, que ressoa pela floresta, é um eco de sofrimento, refletindo a dor de quem perdeu sua identidade. Ao enfrentá-lo, o herói não combate apenas um monstro, mas também suas próprias sombras, tentando entender e, quem sabe, libertar a alma atormentada que ainda persiste na besta."
             AtqNormal={Claw}
             AtqEspecial={Bite}
             ImgCenario={m1}
             Video={VideoLobo}
+            descAtaq={"Ataque curta distância com garras"}
+            descAtaqSp={"Mordida com grande raio"}
           />
           <CardMonster
             imageSrc={Frank}
             altText="Imagem do Frankenstein"
             TextCard="Frankenstein"
             DescCard="Mesmo sem poderes especiais, você se garante no mano-a-mano contra o Frankenstein? Teste e descubra seus limites."
-            DescModal="Em uma pacífica cidade americana, seu desafio agora é um dos maiores monstros já registrados: O Frankenstein. Teste todos os seus limites aproveitando ao máximo o personagem."
+            DescModal="Em uma pacífica cidade americana, o protagonista agora se vê diante de um dos maiores monstros já registrados na história da mitologia: o Frankenstein. Criado por um cientista obcecado pela ideia de ressuscitar a vida, o monstro de Frankenstein é uma figura de grande complexidade, não apenas uma criatura de carne e costura, mas também um ser que luta contra sua própria existência, rejeitado tanto por seu criador quanto pela sociedade. No confronto, o protagonista não só deve enfrentar a força bruta da criatura, mas também testar seus limites emocionais e psicológicos, confrontando questões de criação e identidade. Uma batalha contra os conceitos de humanidade e monstruosidade."
             AtqNormal={cer}
             AtqEspecial={mao}
             ImgCenario={m2}
+            Video={VideoLobo}
+            descAtaq={"Arremesso de um cérebro humano"}
+            descAtaqSp={"Mãos surgem da terra para ataca-lo"}
           />
           <CardMonster
             imageSrc={Vamp}
             altText="Imagem do Drácula"
             TextCard="Drácula"
             DescCard="Este monstro não precisa de apresentações, o terceiro chefe de Helsing botará você em seu devido lugar."
-            DescModal="Em um castelo sombrio e decadente, o lendário Drácula espera pacientemente pelo próximo caçador que ousar desafiá-lo. Com habilidades sobrenaturais, este chefe testa sua capacidade de adaptação e força em combates intensos. Prepare-se para um duelo onde cada movimento é crucial!"
+            DescModal="O Drácula é uma das figuras mais icônicas da mitologia vampírica, um ser que transcende o tempo e o espaço. No mundo de Helsing, ele é o mestre das trevas, manipulando a escuridão e a morte para seu próprio prazer. Sua presença é uma ameaça constante, capaz de controlar criaturas noturnas e dominar a mente dos mais fracos. Enfrentar Drácula é mais do que apenas uma luta física; é um teste de resistência mental e espiritual, onde apenas os mais corajosos podem esperar sobreviver."
             AtqNormal={mor}
             AtqEspecial={rolo}
             ImgCenario={m3}
+            Video={VideoLobo}
+            descAtaq={"Transformação em morcegos"}
+            descAtaqSp={"Cuidado com o Rolo-Compressor!"}
           />
           <CardMonster
             imageSrc={Dragao}
             altText="Imagem do Dragão"
-            TextCard="Dragão de Fogo"
+            TextCard="Dragão"
             DescCard="O desafio final está à sua frente. O poderoso Dragão de Fogo testa seu valor como caçador de monstros. Está preparado para enfrentar a fúria das chamas?"
-            DescModal="Nas profundezas de uma caverna vulcânica, o Dragão de Fogo aguarda o momento em que um guerreiro destemido o desafie. Seu poder avassalador e seus ataques de fogo implacáveis fazem deste o desafio final de Helsing. Mantenha-se firme e derrote o dragão para reivindicar a vitória definitiva!"
+            DescModal="Nas profundezas de uma caverna vulcânica, o Dragão de Fogo aguarda, pronto para testar qualquer guerreiro destemido. Com seu poder avassalador e chamas implacáveis, ele representa o ápice da destruição. Mais do que um monstro, o dragão é o teste final de coragem, desafiando não só a força, mas a determinação do herói. Derrotá-lo é enfrentar o próprio inferno e conquistar a vitória definitiva."
             AtqNormal={fogo}
             AtqEspecial={gelo}
             ImgCenario={m4}
+            Video={VideoLobo}
+            descAtaq={"Um ataque à distância que deixa queimaduras"}
+            descAtaqSp={"Esquive-se dos gelos do grande dragão elemental"}
           />
         </section>
       </main>
     </div>
   );
 }
-
+ 
 export default Bosses;
+ 
--- src/components/Manual.jsx
+++ src/components/Manual.jsx
@@ -14,11 +14,11 @@ function Manual() {
         valiosas!
       </p>
       <div className="MainTxt flex justify-center p-10 bg-black">
-        <div className="cont grid grid-cols-1 md:grid-cols-2 w-full max-w-5xl ps-20">
+        <div className="cont grid grid-cols-1 md:grid-cols-2 w-full max-w-5xl md:ps-20">
           <h1 className="RTDyans text-white text-5xl md:text-6xl md:border-e flex justify-center">
             <span className="C text-7xl">M</span>anual
           </h1>
-          <div className="bttn flex justify-center items-center md: pe-20">
+          <div className="bttn flex justify-center items-center md:pe-20">
             <button
               onClick={handleDownload}
               className="btnDownload text-white p-4 px-8 rounded-lg bg-blue-600 hover:bg-blue-700 transition duration-300 ease-in-out"
